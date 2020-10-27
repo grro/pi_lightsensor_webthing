a517ab91dbbf588d2868122b9b195195531b99a0
grro <gregor.roth@1und1.de>
2020-10-27 15:22:00 +0100

    test

--- pi_lightsensor_webthing/lightsensor_webthing.py
+++ pi_lightsensor_webthing/lightsensor_webthing.py
@@ -13,7 +13,7 @@ class LightSensor(Thing):
         Thing.__init__(
             self,
             'urn:dev:ops:illuminanceSensor-1',
-            'Illuminance Sensor' + name,
+            'Illuminance Sensor ' + name,
             ['BinarySensor'],
             description
         )
